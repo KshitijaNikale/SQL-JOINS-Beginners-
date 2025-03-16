017e550bbd197c0436b3857fc201ec86ece42c8a
Kshitija Nikale <161917361+KshitijaNikale@users.noreply.github.com>
2025-03-16 13:23:29 +0530

    SQL JOINS

---
+++ 16_sql_inner_join_student_db.sql
@@ -0,0 +1,39 @@
+USE Student_db;
+CREATE TABLE IF NOT EXISTS Students (
+Roll_No INT PRIMARY KEY,
+Name VARCHAR(100),
+Age int
+);
+CREATE TABLE IF NOT EXISTS Course (
+Course_ID INT PRIMARY KEY,
+Course_Name VARCHAR(100),
+Roll_No INT,
+foreign key (Roll_no) REFERENCES 
+Students(Roll_no)
+);
+SHOW TABLES;
+INSERT INTO Students ( Roll_No, Name, Age)
+VALUES 
+(20,'Sofia',18),
+(21,'Alice', 18),
+(24, 'Bob', 19);
+
+INSERT INTO Course (Course_ID, Course_Name, Roll_No)
+Values
+(101,'AI&DS',20),
+(102,'Computer Science',21),
+(103,'Robobtics',24);
+SHOW TABLES;
+SELECT *FROM Students;
+SELECT * FROM Course;
+SELECT Students.Roll_no, Students.Name,  Course.Course_ID, course.Course_Name
+FROM Students
+INNER JOIN Course
+ON Students.Roll_No = Course.Roll_No;
+
+
+
+
+
+
+
